9eb96db2663f857d01b2a7f413bd210e45ac534b
nvdtrieu99 <ngtrieu.99@gmail.com>
2021-04-18 17:53:39 +0700

    add all

---
+++ app/api.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Body
+from fastapi.encoders import jsonable_encoder
+# from tensorflow.keras.models import load_model
+# import numpy as np
+# from PIL import Image, ExifTags
+# import cv2
+
+app = FastAPI()
+
+@app.get("/", tags=["Root"])
+async def get_root() -> dict:
+    return {
+        "message": "Welcome to your Okteto app."
+    }
