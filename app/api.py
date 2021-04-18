cdc04f140a524bc48a5b115f36f2cf4057b14425
nvdtrieu99 <ngtrieu.99@gmail.com>
2021-04-18 18:06:47 +0700

    add all

--- app/api.py
+++ app/api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Body
 from fastapi.encoders import jsonable_encoder
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.models import load_model
 # import numpy as np
 # from PIL import Image, ExifTags
 # import cv2
